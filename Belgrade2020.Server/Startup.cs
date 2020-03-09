557107f2bed0bab0f20ee4f2a894d062e28c7f98
Branko Bogdanovic <branko.bogd@gmail.com>
2020-03-09 20:00:06 +0100

    Customize Swagger data with project title, description and author contact.

--- Belgrade2020.Server/Startup.cs
+++ Belgrade2020.Server/Startup.cs
@@ -19,7 +19,22 @@ namespace Belgrade2020.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddOpenApiDocument();
+            services.AddOpenApiDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Belgrade2020 API";
+                    document.Info.Description = 
+                        "A simple ASP.NET Core web API backend for Blgrade2020 app. <br>" +
+                        "Created as an project assignment for 'User Interface Programming' exam by Branko Bogdanovic (br.ind. 19/3372)";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Branko Bogdanovic",
+                        Email = "branko.bogd@gmail.com"
+                    };
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
