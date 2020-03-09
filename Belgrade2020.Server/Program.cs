b093a805e49142e48690039f8c072cb99d281d3b
Branko Bogdanovic <branko.bogd@gmail.com>
2020-03-09 20:58:38 +0100

    Add Serilog logging support.

--- Belgrade2020.Server/Program.cs
+++ Belgrade2020.Server/Program.cs
@@ -1,18 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
 
 namespace Belgrade2020.Server
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.WithProperty("Belgrade2020", "Server API")
+                .WriteTo.File(
+                    Path.Combine("Logs", "log.txt"),
+                    rollingInterval: RollingInterval.Hour,
+                    rollOnFileSizeLimit: true,
+                    shared: true)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +38,7 @@ namespace Belgrade2020.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
